4a6285e61768a8d922117c3967afe6463241d6fb
alvaropinot <alvaro@neatnait.com>
2016-07-16 12:42:09 +0200

    feat(lib): cook some emojis 🍻

---
+++ index.js
@@ -0,0 +1,25 @@
+const {eat, cook, isVegetarian} = require('./lib');
+
+['🍿', '🍳'].reduce(eat);
+// => 💩
+
+['🌽', '🐮', '🐔'].map(cook);
+// => [🍿, 🍔, 🍳]
+
+['🍿', '🍔', '🍳'].filter(isVegetarian);
+// =>  [🍿, 🍳]
+
+/**
+ * Original tweet
+ * @author Steven Luscher
+ * @see https://twitter.com/steveluscher/status/741089564329054208
+ *
+ * map([🌽, 🐮, 🐔], cook)
+ * // => [🍿, 🍔, 🍳]
+ *
+ * filter([🍿, 🍔, 🍳], isVegetarian)
+ * // =>  [🍿, 🍳]
+ *
+ * reduce([🍿, 🍳], eat)
+ * // => 💩
+ */
